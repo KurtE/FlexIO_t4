dacbd69ee59d89fd3ccaf9451751088abb5c284f
Kurt Eckhardt <kurte@rockisland.com>
2019-04-25 07:10:21 -0700

    T4-Beta 2 board edit tables and add FlexIO3...
    Edited the tables, to hopefully start working with Beta 2 board, and should still work with Beta1 board.

--- FlexIO_t4.cpp
+++ FlexIO_t4.cpp
@@ -41,7 +41,57 @@ extern void IRQHandler_FlexIO2();
 
 FlexIOHandlerCallback *flex1_Handler_callbacks[4] = {nullptr, nullptr, nullptr, nullptr};
 FlexIOHandlerCallback *flex2_Handler_callbacks[4] = {nullptr, nullptr, nullptr, nullptr};
+#if defined(__IMXRT1062__)
+extern void IRQHandler_FlexIO3();
+FlexIOHandlerCallback *flex3_Handler_callbacks[4] = {nullptr, nullptr, nullptr, nullptr};
+#endif
+//-----------------------------------------------------------------------------
+// T4 Beta 2 board and probably release
+//-----------------------------------------------------------------------------
 
+#if defined(__IMXRT1062__)
+const FlexIOHandler::FLEXIO_Hardware_t FlexIOHandler::flex1_hardware = {
+	CCM_CCGR5, CCM_CCGR5_FLEXIO1(CCM_CCGR_ON),
+	IRQ_FLEXIO1, 
+	&IRQHandler_FlexIO1,
+	DMAMUX_SOURCE_FLEXIO1_REQUEST0, DMAMUX_SOURCE_FLEXIO1_REQUEST1, DMAMUX_SOURCE_FLEXIO1_REQUEST2, DMAMUX_SOURCE_FLEXIO1_REQUEST3,
+
+	2,       3,    4,    5,  33,  0xff, 0xff, 0xff, 0xff,
+	4,       5,    6,    8,  7,   0xff, 0xff, 0xff, 0xff,
+	0x14, 0x14, 0x14, 0x14, 0x14, 0xff, 0xff, 0xff, 0xff,
+};
+
+const FlexIOHandler::FLEXIO_Hardware_t FlexIOHandler::flex2_hardware = {
+	CCM_CCGR3, CCM_CCGR3_FLEXIO2(CCM_CCGR_ON),
+	IRQ_FLEXIO2, 
+	IRQHandler_FlexIO2,
+	DMAMUX_SOURCE_FLEXIO2_REQUEST0, DMAMUX_SOURCE_FLEXIO2_REQUEST1, DMAMUX_SOURCE_FLEXIO2_REQUEST2, DMAMUX_SOURCE_FLEXIO2_REQUEST3,
+	6,       7,    8,    9,  10,    11,   12,   13,   32,
+	10,     17,   16,   11,  0,      2,    1,    3,   12,
+	0x14, 0x14, 0x14, 0x14, 0x14, 0x14, 0x14, 0x14, 0x14,
+};
+
+const FlexIOHandler::FLEXIO_Hardware_t FlexIOHandler::flex3_hardware = {
+	CCM_CCGR7, CCM_CCGR7_FLEXIO3(CCM_CCGR_ON),
+	IRQ_FLEXIO3, 
+	IRQHandler_FlexIO3,
+	0xff, 0xff, 0xff, 0xff,  // No DMA Sources? 
+	6,       7,    8,    9,  10,    11,   12,   13,   32,
+	10,     17,   16,   11,  0,      2,    1,    3,   12,
+	0x14, 0x14, 0x14, 0x14, 0x14, 0x14, 0x14, 0x14, 0x14,
+};
+
+
+static FlexIOHandler flexIO1((uintptr_t)&IMXRT_FLEXIO1_S, (uintptr_t)&FlexIOHandler::flex1_hardware, (uintptr_t)flex1_Handler_callbacks);
+static FlexIOHandler flexIO2((uintptr_t)&IMXRT_FLEXIO2_S, (uintptr_t)&FlexIOHandler::flex2_hardware, (uintptr_t)flex2_Handler_callbacks);
+static FlexIOHandler flexIO3((uintptr_t)&IMXRT_FLEXIO3, (uintptr_t)&FlexIOHandler::flex3_hardware, (uintptr_t)flex3_Handler_callbacks);
+
+static FlexIOHandler *flex_list[] = {&flexIO1, &flexIO2, &flexIO3};
+
+//-----------------------------------------------------------------------------
+// T4 Beta 1 board
+//-----------------------------------------------------------------------------
+#else
 const FlexIOHandler::FLEXIO_Hardware_t FlexIOHandler::flex1_hardware = {
 	CCM_CCGR5, CCM_CCGR5_FLEXIO1(CCM_CCGR_ON),
 	IRQ_FLEXIO1, 
@@ -68,6 +118,7 @@ static FlexIOHandler flexIO1((uintptr_t)&IMXRT_FLEXIO1_S, (uintptr_t)&FlexIOHand
 static FlexIOHandler flexIO2((uintptr_t)&IMXRT_FLEXIO2_S, (uintptr_t)&FlexIOHandler::flex2_hardware, (uintptr_t)flex2_Handler_callbacks);
 
 static FlexIOHandler *flex_list[] = {&flexIO1, &flexIO2};
+#endif
 
 //-----------------------------------------------------------------------------
 // Interrupt functions
@@ -96,6 +147,18 @@ void IRQHandler_FlexIO2() {
 	 asm("dsb");
 }
 
+#if defined(__IMXRT1062__)
+void IRQHandler_FlexIO3() {
+	FlexIOHandlerCallback **ppfhc = flex3_Handler_callbacks;
+	for (uint8_t i = 0; i < 4; i++) {
+		if (*ppfhc) {
+			if ((*ppfhc)->call_back(&flexIO3)) return;
+		}
+	}
+	flexIO2.IRQHandler();
+	 asm("dsb");
+}
+#endif
 
 //-----------------------------------------------------------------------------
 // Map IO pins to their Flex object and the flex pin 
