f599f0a13034bd310ce2ce0b080f32086cde46c0
Kurt Eckhardt <kurte@rockisland.com>
2021-04-14 08:56:26 -0700

    Fix FlexSerial_Transmit to not crash plus setup to test MicroMod Teensy

--- examples/FlexSerial_Transmit/FlexSerial_Transmit.ino
+++ examples/FlexSerial_Transmit/FlexSerial_Transmit.ino
@@ -1,26 +1,39 @@
 //============================================================================
-// Simple output only test for Flex Serial pin defined on Pin 2
-// Idea is to connect pin 0 (Serail1 RX pin) to Pin2 (Flex IO output)
+// Simple output only test for Flex Serial pin defined on Pin 4
+// Idea is to connect pin 38 (Serail3 RX pin) to Pin4 (Flex IO output)
 // 
 // And the program will hopefully echo the stuff to The serial output
 //============================================================================
-
+// Currently setup to test Teensy Micromod, on Machine Learning connet D0 to A1
 #include <FlexIO_t4.h>
 #include <FlexSerial.h>
 
-FlexSerial SerialFlex(-1, 2); // currently setup for pin 2 TX
+FlexSerial SerialFlex(-1, 4); // currently setup for pin 4 to test MicroMod (D0)
 
 
 void setup() {
   pinMode(13, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
   while (!Serial && millis() < 4000);
+
   Serial.begin(115200);
-  Serial1.begin(115200);  // lets start up Serial1, to see if we can receive anything from our FlexSerial
+  delay(250);
+  Serial.println("Start Test Serial Flex"); Serial.flush();
+
+  Serial3.begin(115200);  // lets start up Serial3, to see if we can receive anything from our FlexSerial
   delay(500);
+  Serial.println("Before SerialFlex.begin"); Serial.flush();
   SerialFlex.begin(115200);
+  Serial.println("After SerialFlex.begin"); Serial.flush();
+
+  // lets enable all of the FlexIO objects to see their parameters.  
+  CCM_CCGR5 |= CCM_CCGR5_FLEXIO1(CCM_CCGR_ON);
+  CCM_CCGR3 |= CCM_CCGR3_FLEXIO2(CCM_CCGR_ON);
+  CCM_CCGR7 |= CCM_CCGR7_FLEXIO3(CCM_CCGR_ON);
+
+  Serial.printf("FLEXIO1_PARAM: %x\n", FLEXIO1_PARAM); Serial.flush();
+  Serial.printf("FLEXIO2_PARAM: %x\n", FLEXIO2_PARAM); Serial.flush();
+  Serial.printf("FLEXIO3_PARAM: %x\n", FLEXIO3_PARAM); Serial.flush();
+
 
   Serial.println("End Setup");
 }
@@ -33,9 +46,9 @@ void loop() {
 
 }
 
-void serialEvent1() {
+void serialEvent3() {
   int ch;
-  while ((ch = Serial1.read()) != -1) {
+  while ((ch = Serial3.read()) != -1) {
     Serial.write(ch);
   }
 }
